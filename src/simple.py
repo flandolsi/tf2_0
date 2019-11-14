92bae6193b938a0403a44290a034f8f2551d3373
flandolsi <flandmail@gmail.com>
2019-11-14 10:18:21 -0600

    Improve logging

--- src/simple.py
+++ src/simple.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
+import logging
+import os
+
+
+# set level for logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+logging.basicConfig(level=logging.INFO)
+
 
 
 (x_train,y_train), (x_test,y_test) = tf.keras.datasets.mnist.load_data()
+logging.info('Data Loaded')
 
 x_train,x_test = x_train / 255.0 , x_test / 255.0
 
@@ -18,8 +27,12 @@ model.compile(
         optimizer='adam',
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy'])
+logging.info('Model Compiled')
+
 
 model.fit(x_train,y_train,epochs=5)
+logging.info('Model trained')
+
 
 model.evaluate(x_test,y_test,verbose=2)
 
